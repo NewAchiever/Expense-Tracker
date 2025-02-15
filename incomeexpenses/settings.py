684e5874ea0867c3d6e5620a6198c8ad04101ade
bnpandya <kaizokugarinozoro98@gmail.com>
2025-02-16 01:59:51 +0530

    added 404 template

--- incomeexpenses/settings.py
+++ incomeexpenses/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-7lo^7&$rl$7i4e##bgc%clfynfj%&tpd&s*5@^fa9k+ez8m7mk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
