cc9c70cece3f082dba17adc3b55619e68dd131fb
bnpandya <kaizokugarinozoro98@gmail.com>
2025-11-01 18:20:09 -0400

    venv activation

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,11 @@
+<<<<<<< HEAD
 source ./django-app/Scripts/activate
 pip install -r requirements.txt
 python3.11 manage.py collectstatic --noinput
+=======
+#!/bin/bash
+python3 -m pip install --upgrade pip
+pip3 install -r requirements.txt
+python3 manage.py collectstatic --noinput
+python3 manage.py runserver
+>>>>>>> f794638fe70e505533610e788de92e87da6a4458
