42373ae2935412bca0eb49bdc75d163291150b70
bnpandya <32551654+NewAchiever@users.noreply.github.com>
2025-11-18 02:23:50 -0500

    Fix merge conflict in build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 source ./django-app/Scripts/activate
 pip install -r requirements.txt
 python3.11 manage.py collectstatic --noinput
@@ -8,4 +8,4 @@ python3 -m pip install --upgrade pip
 pip3 install -r requirements.txt
 python3 manage.py collectstatic --noinput
 python3 manage.py runserver
->>>>>>> f794638fe70e505533610e788de92e87da6a4458
+
