f51d1b5256ccc36d1a902358863e066e9283d5dd
bnpandya <32551654+NewAchiever@users.noreply.github.com>
2025-11-18 11:24:43 -0500

    Fix source command for virtual environment activation

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 python3 -m venv django-app
-source ./django-app/Scripts/activate
+source django-app/Scripts/activate
 
 python3 -m pip install --upgrade pip
 pip install -r requirements.txt
