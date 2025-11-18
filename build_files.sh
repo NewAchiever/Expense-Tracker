d19da19a7d8d49f0442d15b3ad3d391b04049905
bnpandya <32551654+NewAchiever@users.noreply.github.com>
2025-11-18 12:08:41 -0500

    Add activation message for Django virtual environment
    Add confirmation message upon activating the Django virtual environment.

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 python3 -m venv django-app
 source django-app/bin/activate
-
+echo "🎉 Django virtual environment 'django-app' activated! 🎉"
 python3 -m pip install --upgrade pip
 pip install -r requirements.txt
 
