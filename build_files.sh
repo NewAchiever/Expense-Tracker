8d2d90f1d3687e1fe4c518fa39fe41adfad8228a
bnpandya <32551654+NewAchiever@users.noreply.github.com>
2025-11-18 12:22:51 -0500

    Remove unnecessary package installations
    Removed package installation commands from build script.

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-apt-get update
-apt-get install libpq-dev python3-dev gcc
-
 python3 -m venv django-app
 source django-app/bin/activate
 echo "🎉 Django virtual environment 'django-app' activated! 🎉"
