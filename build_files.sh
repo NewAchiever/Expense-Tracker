d04b1f4ca01e72b4860af04a20f4edc9ec7fdcd0
bnpandya <32551654+NewAchiever@users.noreply.github.com>
2025-11-18 02:35:33 -0500

    Refactor build script for Django application setup

--- build_files.sh
+++ build_files.sh
@@ -1,11 +1,10 @@
-
-source ./django-app/Scripts/activate
-pip install -r requirements.txt
-python3.11 manage.py collectstatic --noinput
-=======
 #!/bin/bash
+python3 -m venv django-app
+source ./django-app/Scripts/activate
+
 python3 -m pip install --upgrade pip
-pip3 install -r requirements.txt
+pip install -r requirements.txt
+
 python3 manage.py collectstatic --noinput
 python3 manage.py runserver
 
