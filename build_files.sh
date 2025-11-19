35f9a9a8d3dc0f59265c5178bd86f87f44e91b57
bnpandya <32551654+NewAchiever@users.noreply.github.com>
2025-11-19 10:41:03 -0500

    Add database migration commands to build script

--- build_files.sh
+++ build_files.sh
@@ -6,5 +6,7 @@ python3 -m pip install --upgrade pip
 pip install -r requirements.txt
 
 python3 manage.py collectstatic --noinput
+python3 manage.py makemigrations
+python3 manage.py migrate
 python3 manage.py runserver
 
