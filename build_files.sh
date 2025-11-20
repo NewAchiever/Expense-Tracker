e5e752f063c4b7cbea9d62c3aa3c365c55148021
bnpandya <32551654+NewAchiever@users.noreply.github.com>
2025-11-20 07:08:36 -0500

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -8,8 +8,3 @@ pip install -r requirements.txt
 python3 manage.py collectstatic --noinput
 python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
-
-python3 manage.py dbshell
-
-python3 manage.py runserver 
-
